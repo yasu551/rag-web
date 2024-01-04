766d99a6071ec5e25c5a05b1c86a15ed38efab1c
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-01-05 02:44:38 +0900

    llmに返答させられるように

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from 'hono'
+import { Ai } from '@cloudflare/ai'
+import { stream, streamText } from 'hono/streaming'
 
-const app = new Hono()
+type Bindings = {
+  AI: any
+}
+
+type Answer = {
+  response: string
+}
+
+type Message = {
+  content: string
+  role: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/ai', async (c) => {
+  const ai = new Ai(c.env.AI)
+  const answer: Answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages: [
+      {
+        role: 'user',
+        content: `What is Cloudflare Workers. You respond in less than 100 words.`
+      }
+    ]
+  })
+  const strings = [...answer.response]
+  return streamText(c, async (stream) => {
+    for (const s of strings) {
+      stream.write(s)
+      await stream.sleep(10)
+    }
+  })
 })
 
 export default app
