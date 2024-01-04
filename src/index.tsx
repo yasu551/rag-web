7767f70e830ac2438a98b26d37c9c034bbc35fef
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-01-05 07:24:33 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -49,6 +49,7 @@ app.get('/', (c) => {
           name='similarityCutoff'
           min='0'
           value='0'
+          stpe='0.01'
           style={{
             width: '100%'
           }}          
