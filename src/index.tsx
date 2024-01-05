68cb6c950a1e7e650c275d36e116028f735ffa00
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-01-05 09:53:32 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -114,18 +114,18 @@ app.post('/ai', async (c) => {
       ...messages,
     ]
   })
-  const response = await ai.run('@cf/meta/m2m100-1.2b', {
-    text: answer.response,
-    source_lang: 'english',
-    target_lang: 'japanese'
-  })  
+  // const response = await ai.run('@cf/meta/m2m100-1.2b', {
+  //   text: answer.response,
+  //   source_lang: 'english',
+  //   target_lang: 'japanese'
+  // })  
   return c.json({
     systemMessage: systemMessage.content,
     contextMessage: contextContent,
     vectorQuery: vectorQuery,
     messages: messages.map(m => m.content),
-    answerMessage: response,
-    response: response,
+    answerMessage: answer.response,
+    // response: response,
   })
 })
 
